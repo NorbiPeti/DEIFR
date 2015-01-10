05116b846865909b0b08c88542cdb8e337be0eda
norbipeti <norbipeti@67d14424-0591-4828-bdd5-a4795eeb194f>
2015-01-10 21:59:14 +0000

    Initial upload. :P

---
+++ DEIFR/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEIFR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.Value = Program.MaxImages;
+            checkBox1.Checked = Program.KeepImages;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Program.KeepImages = checkBox1.Checked;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            Program.MaxImages = (int)numericUpDown1.Value;
+            numericUpDown1.Value = Program.MaxImages; //A way to disallow non-integer inputs
+        }
+    }
+}
---
+++ DEIFR/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEIFR
+{
+    static class Program
+    {
+        public static bool KeepImages;
+        public static int MaxImages;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (File.Exists("DEIFRSettings.ini"))
+            {
+                string[] ss = File.ReadAllLines("DEIFRSettings.ini");
+                foreach (var s in ss)
+                {
+                    string[] x = s.Split('=');
+                    switch (x[0])
+                    {
+                        case "keepimages":
+                            KeepImages = bool.Parse(x[1]);
+                            break;
+                        case "maximages":
+                            MaxImages = int.Parse(x[1]);
+                            break;
+                    }
+                }
+            }
+            ImageDownloader.Update();
+            if (args.Length == 0)
+                Application.Run(new Form1());
+            else if (args[0] != "silent")
+                Console.WriteLine("Error: Unknown parameter(s). Use \"silent\" to open in background, otherwise don't give any parameters to show settings.");
+            List<string> sw = new List<string>();
+            sw.Add("maximages=" + MaxImages);
+            sw.Add("keepimages=" + KeepImages);
+            File.WriteAllLines("DEIFRSettings.ini", sw);
+        }
+    }
+}
