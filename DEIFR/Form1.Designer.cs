261c67fe9b9becafc34b9d44a574884d95d61233
NorbiPeti <szatmari.norbert.peter@gmail.com>
2015-01-10 21:59:14 +0000

    Initial upload. :P

---
+++ DEIFR/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace DEIFR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(304, 13);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Maximum number of images:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 33);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(123, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Keep current images";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(279, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "It will remove all unrelated files too if you uncheck this box";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(196, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Settings are saved automatically on exit.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(405, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "If you want to automatically start the program on boot, provide the \"silent\" pare" +
+    "meter.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 118);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "DEIFR - Download Earth Images From Reddit";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DEIFR/ImageDownloader.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DEIFR
+{
+    public static class ImageDownloader
+    {
+        public static void Update()
+        {
+            if (!Directory.Exists("wallpapers"))
+                Directory.CreateDirectory("wallpapers");
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://www.reddit.com/r/ImagesOfEarth/.json?limit=" + Program.MaxImages + "&sort=new");
+            //string senddata = "?limit=15&sort=new";
+            //byte[] sendbytes = Encoding.Unicode.GetBytes(senddata);
+            req.Method = "GET";
+            //req.ContentLength = sendbytes.Length;
+            req.ContentType = "text/plain";
+            /*var sendstr = req.GetRequestStream();
+            sendstr.Write(sendbytes, 0, sendbytes.Length);
+            sendstr.Close();*/
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            Stream respstr = resp.GetResponseStream();
+            string response = new StreamReader(respstr).ReadToEnd();
+            JObject obj = JObject.Parse(response);
+            List<JToken> data = obj["data"]["children"].Children().ToList();
+            int i = 0;
+            List<string> files = Directory.GetFiles("wallpapers").ToList();
+            foreach (JToken result in data)
+            {
+                string s = result["data"]["url"].ToString();
+                string id = result["data"]["id"].ToString();
+                if (files.Any(entry => entry.Contains(id)))
+                {
+                    files.RemoveAll(entry => entry.Contains(id)); //Leave only the files that wasn't processed to remove them later
+                    continue; //Don't download twice
+                }
+                if (!s.Contains(".jpg"))
+                {
+                    if (s.Contains("imgur.com"))
+                        s += ".jpg"; //On imgur.com it is enough to get image
+                    else
+                        continue; //Otherwise we don't know what is there
+                }
+                //Image.FromStream(respstr).Save(i + "." + s.Split('/').Last().Split('.')[1]);
+                string path = id + "." + s.Split('/').Last().Split('.')[1];
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(s, "wallpapers" + Path.DirectorySeparatorChar + path);
+                }
+                files.Remove(path);
+                i++;
+            }
+            if (!Program.KeepImages)
+            {
+                foreach (string file in files)
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+    }
+}
